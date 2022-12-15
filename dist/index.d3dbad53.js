529bee861784062734fd03201aea98278f3a61f1
Reizlina Tanya <97384405+Reizlina@users.noreply.github.com>
2022-12-15 18:04:21 +0200

    feat(scroll): add redirect on scroll
    Need animation and tests

---
+++ dist/index.d3dbad53.js
@@ -0,0 +1,34 @@
+const tri = document.querySelector(".hero__thumb-triatlon");
+const run = document.querySelector(".hero__thumb-run");
+let touchstartX = 0;
+let touchendX = 0;
+let touchstartY = 0;
+let touchendY = 0;
+function checkDirection() {
+    let difX = touchstartX - touchendX;
+    let difY = touchstartY - touchendY;
+    if (Math.abs(difX) > Math.abs(difY)) {
+        if (touchendX < touchstartX) /*left*/ return;
+        if (touchendX > touchstartX) /*right*/ return;
+    } else {
+        if (touchendY < touchstartY) /*up*/ //   console.log(run);
+        document.location.href = "../run.html";
+        if (touchendY > touchstartY) /*down*/ //   console.log(tri);
+        document.location.href = "../triatlon.html";
+    }
+}
+document.addEventListener("touchstart", (e)=>{
+    //   e.preventDefault();
+    touchstartX = e.changedTouches[0].screenX;
+    touchstartY = e.changedTouches[0].screenY;
+});
+document.addEventListener("touchend", (e)=>{
+    //   e.preventDefault();
+    //   console.log('e :>> ', e.target);
+    touchendX = e.changedTouches[0].screenX;
+    touchendY = e.changedTouches[0].screenY;
+    if (e.target === tri || e.target === run) checkDirection();
+    return;
+});
+
+//# sourceMappingURL=index.d3dbad53.js.map

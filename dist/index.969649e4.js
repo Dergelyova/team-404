b5dd791801dcb71521bc15da11285f215ce2bfa8
Reizlina Tanya <97384405+Reizlina@users.noreply.github.com>
2022-12-16 15:43:03 +0200

    feat(hero): add swiper with finish-points

--- dist/index.969649e4.js
+++ dist/index.969649e4.js
@@ -1,2 +1,35 @@
+const tri = document.querySelector(".hero__thumb-triatlon");
+const run = document.querySelector(".hero__thumb-run");
+tri.addEventListener("touchstart", ()=>{
+    tri.addEventListener("touchend", endFncTri);
+}, true);
+const endFncTri = (e)=>{
+    let y = e.changedTouches[0].screenY;
+    console.log("y :>> ", y);
+    if (y > 550) document.location.href = "../triatlon.html";
+    return;
+};
+run.addEventListener("touchstart", ()=>{
+    run.addEventListener("touchend", endFncRun);
+}, true);
+const endFncRun = (e)=>{
+    let y = e.changedTouches[0].screenY;
+    //   console.log('window.requestAnimFrame :>> ', window.requestAnimationFrame);
+    //   console.log('y :>> ', y);
+    if (y < 300) document.location.href = "../run.html";
+    return;
+}; // const onAnimFrame = () => {
+ //   //   if (!rafPending) {
+ //   //     return;
+ //   //   }
+ //   let differenceInX = initialTouchPos.x - lastTouchPos.x;
+ //   let newXTransform = currentXPosition - differenceInX + 'px';
+ //   let transformStyle = 'translateX(' + newXTransform + ')';
+ //   swipeFrontElement.style.webkitTransform = transformStyle;
+ //   swipeFrontElement.style.MozTransform = transformStyle;
+ //   swipeFrontElement.style.msTransform = transformStyle;
+ //   swipeFrontElement.style.transform = transformStyle;
+ //   //   rafPending = false;
+ // };
 
 //# sourceMappingURL=index.969649e4.js.map
--- src/js/hero.js
+++ src/js/hero.js
@@ -1,12 +1,54 @@
-// const tri = document.querySelector('.hero__thumb-triatlon');
-// const run = document.querySelector('.hero__thumb-run');
+const tri = document.querySelector('.hero__thumb-triatlon');
+const run = document.querySelector('.hero__thumb-run');
 
-// const timerForScale = () => {
-//   setInterval(() => {
-//     // console.log(tri.style);
-//     // tri.style.height = '90%';
-//     // run.style.height = '10%';
-//   }, 3000);
+tri.addEventListener(
+  'touchstart',
+  () => {
+    tri.addEventListener('touchend', endFncTri);
+  },
+  true
+);
+
+const endFncTri = e => {
+  let y = e.changedTouches[0].screenY;
+  console.log('y :>> ', y);
+  if (y > 550) {
+    document.location.href = '../triatlon.html';
+  }
+  return;
+};
+
+run.addEventListener(
+  'touchstart',
+  () => {
+    run.addEventListener('touchend', endFncRun);
+  },
+  true
+);
+
+const endFncRun = e => {
+  let y = e.changedTouches[0].screenY;
+  //   console.log('window.requestAnimFrame :>> ', window.requestAnimationFrame);
+  //   console.log('y :>> ', y);
+  if (y < 300) {
+    document.location.href = '../run.html';
+  }
+  return;
+};
+
+// const onAnimFrame = () => {
+//   //   if (!rafPending) {
+//   //     return;
+//   //   }
+
+//   let differenceInX = initialTouchPos.x - lastTouchPos.x;
+//   let newXTransform = currentXPosition - differenceInX + 'px';
+//   let transformStyle = 'translateX(' + newXTransform + ')';
+
+//   swipeFrontElement.style.webkitTransform = transformStyle;
+//   swipeFrontElement.style.MozTransform = transformStyle;
+//   swipeFrontElement.style.msTransform = transformStyle;
+//   swipeFrontElement.style.transform = transformStyle;
+
+//   //   rafPending = false;
 // };
-
-// timerForScale();

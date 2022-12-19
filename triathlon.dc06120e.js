5099814a245a0bf19b0f02146c4f560ef2358be3
Reizlina <97384405+Reizlina@users.noreply.github.com>
2022-12-19 13:16:07 +0000

    Deploying to gh-pages from @ Dergelyova/team-404@b4f582faf75fd2420a34e167e968ddd7e7afab01 🚀

--- triathlon.dc06120e.js
+++ triathlon.dc06120e.js
@@ -1,2 +1,2 @@
 !function(){for(var e=document.querySelectorAll(".header__burger, .header__menu"),r=0;r<e.length;r++)e[r].addEventListener("click",(function(e){for(var r=document.querySelectorAll(".header__burger,.header__menu"),t=0;t<r.length;t++)r[t].classList.toggle("active")}))}();
-//# sourceMappingURL=triatlon.9480bf47.js.map
+//# sourceMappingURL=triathlon.dc06120e.js.map

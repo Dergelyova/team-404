a1aed02a7c7f8d57f3f8b54f1f776891f6f75e4f
Reizlina Tanya <97384405+Reizlina@users.noreply.github.com>
2022-12-25 18:19:16 +0200

    feat(tri):create how we succeed section(#32)
    * feat(pagination): adding swiper and created section
    
    Create section to Mobile version
    Pagination in progress
    
    * fix(pagination): in progress
    
    Still don't work
    
    * fix: add some fixes to swiper initialization (#30)
    
    * feat(sucseed): create desktop and tablet version
    
    Also fix positioning in Achievements section
    
    Co-authored-by: Dergelyova <88082907+Dergelyova@users.noreply.github.com>

---
+++ src/js/swiper.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.mySwiper', {
+  direction: 'horizontal',
+  loop: true,
+  speed: 400,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});

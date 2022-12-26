e4db0d69db09226880e7eac4e42f7591015d280a
Dergelyova <88082907+Dergelyova@users.noreply.github.com>
2022-12-26 10:24:03 +0000

    Deploying to gh-pages from @ Dergelyova/team-404@9854de4d9de4778de49205eceeb21cf414a02ecb 🚀

---
+++ triathlon.d4563ae2.js
@@ -0,0 +1,2 @@
+function openTab(e,t){var a,l,n;for(l=document.getElementsByClassName("tabcontent"),a=0;a<l.length;a++)l[a].style.display="none";for(n=document.getElementsByClassName("tablinks"),a=0;a<n.length;a++)n[a].className=n[a].className.replace(" active","");document.getElementById(t).style.display="grid",document.querySelector(".tablinks_".concat(t)).classList.toggle("active")}
+//# sourceMappingURL=triathlon.d4563ae2.js.map

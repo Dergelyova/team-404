c15bcfd59245b6cb8f063c87f190f18b509b053b
Reizlina <97384405+Reizlina@users.noreply.github.com>
2023-01-09 11:52:17 +0000

    Deploying to gh-pages from @ Dergelyova/team-404@608b516ccc75d3d951a06af0a71b6614b0cc8920 🚀

---
+++ triathlon.7f2c27b8.js
@@ -0,0 +1,2 @@
+var swiperModal=new Swiper(".modalSwiper",{direction:"horizontal",loop:!0,speed:400,slidesPerView:1,spaceBetween:30,centeredSlides:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=triathlon.7f2c27b8.js.map
